dd41757063a03632869c18edf05d408658c8983b
Bereket <bereketteshome685@gmail.com>
2023-11-18 08:55:32 -0800

    Configured Vercel

--- server/index.js
+++ server/index.js
@@ -11,14 +11,15 @@ app.use(cors())
 
 //routes
 app.use('/api/recipe', recipeRouter)
+
 app.get("/", (req, res)=>{
-    res.setHeader("Access-Control-Allow-Credentials", "true")
+    res.send("Hello World!")
 })
 
 const start = async () =>{
     const port = 3001 
     try {
-        await connect("mongodb+srv://bereketteshome685:462462b@recipe-web-application.sbpzkj0.mongodb.net/?retryWrites=true&w=majority");
+        await connect(process.env.MONGO_URL);
         app.listen(port, () => console.log(`Server is running on port ${port}`))
     } catch (error) {
         console.error(error)
